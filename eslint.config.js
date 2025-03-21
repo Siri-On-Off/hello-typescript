97cc2195907f0eb45e56e99b5b884f43720b318b
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-03-21 08:58:17 +0100

    missing files

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    // Deine ESLint-Regeln hier
+  },
+};
